05529fa57783f50c529b601e3bfef61518e9ea4b
Akula175 <tobias.gus99@gmail.com>
2022-05-09 14:18:13 +0200

    update 0.1

--- test.py
+++ test.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env/python3
 
-print("HELLO")
+first_name = "Bob"
+
+last_name = "Lakul"
+
+age = 46
+
+active = True
+
+salary = 35000.60
+
+print(first_name)
+print(type(first_name))
+
+print(age)
+print(type(age))
+
+print(active)
+print(type(active))
+
+print(salary)
+print(type(salary))
+
+
+
+#hobby = input("What is your Hobby?: ")
+#print("Your hobby is: " + hobby)
+
+print("Hello \nNewline") # \n inserts newline
+
+print("Hello \tTabbing") # \t inserts tab
+
+print(len(first_name)) # len() Shows length of str
