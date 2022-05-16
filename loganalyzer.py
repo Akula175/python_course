6b0fa1b12cc65f34e17e51b069ad578267c3b3a2
Akula175 <tobias.gus99@gmail.com>
2022-05-16 13:29:53 +0200

    Fixed files

--- loganalyzer.py
+++ loganalyzer.py
@@ -1,4 +1,18 @@
+# Tobias_Gustafsson_tobias.gustafsson@edu.edugrade.se
 # Log reader
+# Input: loganalyzer.py filepath action
+# Possible actions: statistics, error, notice
+
+#statistics:
+#errors 340
+#notice 450
+
+#error
+#date message
+
+#notice
+#date message
+
 import sys
 
 file_path = sys.argv[1]
