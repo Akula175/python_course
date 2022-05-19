6ba861069bc51c4c956344fd6b294e1cb52d98d0
Akula175 <tobias.gus99@gmail.com>
2022-05-19 11:42:39 +0200

    Added try/except, mby not important

--- loganalyzer.py
+++ loganalyzer.py
@@ -15,23 +15,33 @@ def arg_check():
 def stats():
     notice_count = 0
     error_count = 0
-    with open(file_path) as f:
-        for line in f:
-            if "error" in line:
-                error_count +=1
-            if "notice" in line:
-                notice_count +=1
-    print("errors", error_count)
-    print("notice", notice_count)
+    try:
+        with open(file_path) as f:
+            for line in f:
+                if "error" in line:
+                    error_count +=1
+                if "notice" in line:
+                    notice_count +=1
+        print("errors", error_count)
+        print("notice", notice_count)
+    except FileNotFoundError:
+        print("ERROR: File " + file_path + " not found")
+        return False
+    return True
 
 def line_print(t, n):
-    with open(file_path) as f:
-        for line in f:
-            if t in line:
-                date = (line[:26])
-                type = (line[27:35])
-                msg = (line[n:])
-                print(date, msg)
+    try:
+        with open(file_path) as f:
+            for line in f:
+                if t in line:
+                    date = (line[:26])
+                    type = (line[27:35])
+                    msg = (line[n:])
+                    print(date, msg)
+    except FileNotFoundError:
+        print("ERROR: File " + file_path + " not found")
+        return False
+    return True
 
 if __name__ == "__main__":
     arg_check()
